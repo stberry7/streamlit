321d69ce08472e2af01e1fbaa3e36ffd62d654ec
stberry7 <127612366+stberry7@users.noreply.github.com>
2023-08-28 04:26:14 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # URL of the raw CSV file in your GitHub repository
-url = "https://github.com/stberry7/streamlit/blob/main/school.csv"
+url = "https://raw.githubusercontent.com/stberry7/streamlit/main/school.csv"
 
 # Read the CSV file directly in your Streamlit app
 df = pd.read_csv(url)

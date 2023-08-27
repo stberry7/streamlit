8a2483546be8a6f73457b72304c9d4f76327586d
stberry7 <127612366+stberry7@users.noreply.github.com>
2023-08-28 04:35:34 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,25 @@
 import streamlit as st
 import pandas as pd
 
-# URL of the raw CSV file in your GitHub repository
-url = "https://raw.githubusercontent.com/stberry7/streamlit/main/school.csv"
+# Sample data as a dictionary (Replace this with your CSV read)
+data = {
+    'category': ['School1', 'School2', 'School3', 'School4', 'School5', 'School6', 'School7', 'School8', 'School9', 'School10', 'School11'],
+    'Name2': [115, 202, 206, 683, 37, 796, 56, 551, 578, 904, 446],
+    'Name3': [378, 131, 956, 183, 541, 155, 903, 688, 573, 491, 394],
+    'Name4': [439, 35, 635, 861, 510, 540, 864, 632, 609, 391, 139],
+    'Name5': [274, 156, 990, 39, 664, 479, 405, 763, 53, 643, 361]
+}
 
-# Read the CSV file directly in your Streamlit app
-df = pd.read_csv(url)
+# Convert dictionary to DataFrame
+df = pd.DataFrame(data)
+
+# Set 'category' as index
+df.set_index('category', inplace=True)
 
 # Display the DataFrame in Streamlit
 st.write("Displaying the DataFrame:")
 st.write(df)
 
-# Check if the required columns exist in the DataFrame
-if 'School_Name' in df.columns and 'Total_Students' in df.columns:
-    # Plotting a bar chart
-    st.write("Bar chart based on Total Students in each School:")
-    chart_data = df[['School_Name', 'Total_Students']].set_index('School_Name')
-    st.bar_chart(chart_data)
-else:
-    st.write("Could not find the required columns for plotting the bar chart.")
+# Plotting a bar chart
+st.write("Bar chart based on the data:")
+st.bar_chart(df)
